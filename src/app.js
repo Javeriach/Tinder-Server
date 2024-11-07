1be3e9a687413dfa2550d084d4d831d824037373
Javeriach <javeriakanwal383@gmail.com>
2024-11-07 23:25:00 +0500

    Almost All the backend Completed

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use('/test', (req, res) => {
-  res.send('I am present');
-});
-
 app.listen('7777', () => {
   console.log('I am running');
 });
