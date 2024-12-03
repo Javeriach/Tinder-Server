3ee34d4f44db59dd2de8ef17cfbd990d11677807
Javeriach <javeriakanwal383@gmail.com>
2024-12-04 02:57:50 +0500

    change index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 
 app.use(
   cors({
-    origin: ['https://tinder-frontend-code.vercel.app/'],
+    origin: 'https://tinder-frontend-code.vercel.app/',
     credentials: true,
   })
 );
@@ -19,25 +19,20 @@ app.use(express.json());
 app.use(cookieParser());
 
 // CSP Middleware for Enhanced Security
-// CSP Middleware for Enhanced Security
 app.use((req, res, next) => {
+  // Setting a Content Security Policy to allow framing from vercel.live
   res.setHeader(
     'Content-Security-Policy',
     [
-      "default-src 'self';", // Allow resources from the same origin
-      "script-src 'self' 'unsafe-inline';", // Allow inline scripts and scripts from the same origin
-      "style-src 'self' 'unsafe-inline';", // Allow inline styles and styles from the same origin
+      "default-src 'self';", // Default policy: only allow resources from the same origin
+      "script-src 'self' https://vercel.live;", // Allow scripts from self and vercel.live
+      "style-src 'self' 'unsafe-inline';", // Allow styles from self and inline styles
+      "object-src 'none';", // Disallow <object> and <embed> tags
       "img-src 'self' data:;", // Allow images from self and data URIs
-      "connect-src 'self';", // Allow network requests to self
-      "font-src 'self';", // Allow fonts from self
-      "object-src 'none';", // Disallow <object>, <embed>, <applet>
-      "child-src 'none';", // Disallow the loading of any frames or workers
-      "frame-src 'none';", // Disallow embedding in frames or iframes
-      "frame-ancestors 'none';", // Prevent your content from being embedded into other websites
-      "form-action 'self';", // Only allow forms to be submitted to the same origin
-      "manifest-src 'self';", // Allow the manifest file to be loaded only from the same origin
-      "base-uri 'self';", // Restrict base URIs to the same origin
-      'upgrade-insecure-requests;', // Upgrade all HTTP requests to HTTPS
+      "connect-src 'self' https://vercel.live;", // Allow network requests to self and vercel
+      "font-src 'self' data:;", // Allow fonts from self and data URIs
+      "frame-src 'self' https://vercel.live;", // Allow framing of content from self and vercel.live
+      "frame-ancestors 'none';", // Prevent embedding of your app in other frames
     ].join(' ')
   );
 
