2ebb4d324c9c8d76a5ed7e640a8d8602088defdc
Javeriach <javeriakanwal383@gmail.com>
2024-12-04 03:23:39 +0500

    change index.js

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const requestRouter = require('./src/routes/connectionRequest');
 const userRouter = require('./src/routes/usersConnection');
 const cors = require('cors');
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.use(
   cors({
     origin: 'https://tinder-frontend-code.vercel.app',
@@ -15,29 +18,7 @@ app.use(
   })
 );
 
-app.use(express.json());
-app.use(cookieParser());
-
 // CSP Middleware for Enhanced Security
-app.use((req, res, next) => {
-  // Setting a Content Security Policy to allow framing from vercel.live
-  res.setHeader(
-    'Content-Security-Policy',
-    [
-      "default-src 'self';", // Default policy: only allow resources from the same origin
-      "script-src 'self' https://vercel.live;", // Allow scripts from self and vercel.live
-      "style-src 'self' 'unsafe-inline';", // Allow styles from self and inline styles
-      "object-src 'none';", // Disallow <object> and <embed> tags
-      "img-src 'self' data:;", // Allow images from self and data URIs
-      "connect-src 'self' https://vercel.live;", // Allow network requests to self and vercel
-      "font-src 'self' data:;", // Allow fonts from self and data URIs
-      "frame-src 'self' https://vercel.live;", // Allow framing of content from self and vercel.live
-      "frame-ancestors 'none';", // Prevent embedding of your app in other frames
-    ].join(' ')
-  );
-
-  next(); // Pass control to the next middleware
-});
 
 //EXPRESS ROUTERS
 app.use('/', authRouter); // Routes for authentication
